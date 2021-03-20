6e2717f97bcb4b54bb61de535ae83278e5c897fd
Niek Nijland <ngnijland@gmail.com>
2021-03-20 12:05:34 +0100

    feat: init commit

---
+++ Sijbersounz/SijbersounzApp.swift
@@ -0,0 +1,17 @@
+//
+//  SijbersounzApp.swift
+//  Sijbersounz
+//
+//  Created by Niek Nijland on 20/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SijbersounzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
