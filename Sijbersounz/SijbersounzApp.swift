495f516a79383a6b0567d727adc65ba288aa3f6a
Niek Nijland <ngnijland@gmail.com>
2021-03-29 21:36:53 +0200

    feat: background audio keeps playing when playing a sound

--- Sijbersounz/SijbersounzApp.swift
+++ Sijbersounz/SijbersounzApp.swift
@@ -6,9 +6,23 @@
 //
 
 import SwiftUI
+import AVKit
 
 @main
 struct SijbersounzApp: App {
+    init() {
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSession.setCategory(
+                AVAudioSession.Category.playback,
+                options: AVAudioSession.CategoryOptions.mixWithOthers
+            )
+        } catch {
+            print("Could not set audio session category")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
